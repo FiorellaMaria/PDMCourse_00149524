b9a49724e517d5ade1061ed2dbf5dbc3a4bb6554
FiorellaMaria <00149524@uca.edu.sv>
2026-05-20 12:39:03 -0600

    feat: Agregar Modelos

---
+++ labo4/app/src/main/java/com/labo4/models/Task.kt
@@ -0,0 +1,8 @@
+//Model/Task.kt
+data class Task (
+    val id: Int,
+    val title: String,
+    val description: String,
+    val endDate: Date = Date(),
+    val isCompleted: Boolean = false
+)
---
+++ labo4/app/src/main/java/com/labo4/viewmodels/ViewModel.kt
@@ -0,0 +1,8 @@
+class GeneralViewModel: ViewModel() {
+    private val _tasks = MutableStateFlow<MutableList<Task>>(mutableListOf())
+    val tasks = _tasks.asStateFlow()
+
+    fun addTask(task: Task) {
+        _tasks.value = _tasks.value.toMutableList().apply { add(task) }
+    }
+}
